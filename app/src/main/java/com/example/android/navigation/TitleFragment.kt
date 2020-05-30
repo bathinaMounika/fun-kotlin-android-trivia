792b71d6a29f123b615be6c56af410326ac2792f
Bathina Mounika <62980926+bathinaMounika@users.noreply.github.com>
2020-05-30 18:59:19 +0530

    created onClickListener and Find Navigation Controller to navigate with our Action

--- app/src/main/java/com/example/android/navigation/TitleFragment.kt
+++ app/src/main/java/com/example/android/navigation/TitleFragment.kt
@@ -7,6 +7,8 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
 import androidx.databinding.DataBindingUtil
+import androidx.navigation.Navigation
+import androidx.navigation.findNavController
 import com.example.android.navigation.databinding.FragmentTitleBinding
 
 /**
@@ -19,6 +21,10 @@ class TitleFragment : Fragment() {
         val binding: FragmentTitleBinding = DataBindingUtil.inflate(
                 inflater, R.layout.fragment_title, container, false
         )
+        binding.playButton.setOnClickListener {view: View ->
+            //Navigation.findNavController(view).navigate(R.id.action_titleFragment_to_gameFragment)
+            Navigation.findNavController(view).navigate(R.id.action_titleFragment_to_gameFragment)
+        }
         return binding.root
     }
 }
