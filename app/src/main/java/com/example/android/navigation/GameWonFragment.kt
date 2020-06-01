439f4f0215f324d19ebf345d2921f93b2aad1c80
Bathina Mounika <62980926+bathinaMounika@users.noreply.github.com>
2020-06-01 17:31:05 +0530

    changed to Navigations with action id's in GameWonFragmentDirections

--- app/src/main/java/com/example/android/navigation/GameWonFragment.kt
+++ app/src/main/java/com/example/android/navigation/GameWonFragment.kt
@@ -20,6 +20,7 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
 import androidx.navigation.findNavController
@@ -35,6 +36,9 @@ class GameWonFragment : Fragment() {
         binding.nextMatchButton.setOnClickListener { view: View ->
             view.findNavController().navigate(R.id.action_gameWonFragment_to_gameFragment2)
         }
+        var args = GameWonFragmentArgs.fromBundle(arguments!!)
+        Toast.makeText(context,
+        "NumCorrect: ${args.numCorrect}, NumQuestions: ${args.numQuestions}", Toast.LENGTH_LONG).show()
         return binding.root
     }
 }
