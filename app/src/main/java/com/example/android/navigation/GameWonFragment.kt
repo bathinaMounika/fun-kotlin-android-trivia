7b55c5dc4034951eacd5937a6a981eab3ef9995e
Bathina Mounika <62980926+bathinaMounika@users.noreply.github.com>
2020-06-01 18:43:55 +0530

    hide sharing menu item if sharing intent doesn't resolve to an Activity

--- app/src/main/java/com/example/android/navigation/GameWonFragment.kt
+++ app/src/main/java/com/example/android/navigation/GameWonFragment.kt
@@ -20,6 +20,7 @@ import android.content.Intent
 import android.os.Bundle
 import android.view.*
 import android.widget.Toast
+import androidx.core.app.ShareCompat
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
 import androidx.navigation.findNavController
@@ -45,15 +46,26 @@ class GameWonFragment : Fragment() {
     override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
         super.onCreateOptionsMenu(menu, inflater)
         inflater?.inflate(R.menu.winner_menu,menu)
+
+        //check if activity resolves
+        if(null == getShareIntent().resolveActivity(requireActivity().packageManager)){
+                    //hide the menu item if it does not resolve
+                    menu?.findItem(R.id.share)?.setVisible(false)
+                }
     }
 
     private fun getShareIntent(): Intent {
         var args = GameWonFragmentArgs.fromBundle(requireArguments())
-        val shareIntent = Intent(Intent.ACTION_SEND)
+        /*val shareIntent = Intent(Intent.ACTION_SEND)
         shareIntent.setType("text/plain")
                 .putExtra(Intent.EXTRA_TEXT,
                 getString(R.string.share_success_text, args.numCorrect, args.numQuestions))
-        return shareIntent
+        return shareIntent*/
+        //using sourceCompat fluent API
+        return ShareCompat.IntentBuilder.from(activity)
+                .setText(getString(R.string.share_success_text, args.numCorrect, args.numQuestions))
+                .setType("text/plain") //check visibility of the share icon by commenting this line
+                .intent
     }
 
     private fun shareSuccess(){
