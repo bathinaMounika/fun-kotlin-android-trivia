1c59f627d9d7622854ec7b96f6c4288fbf663d68
Bathina Mounika <62980926+bathinaMounika@users.noreply.github.com>
2020-06-07 18:06:04 +0530

    changed questions

--- app/src/main/java/com/example/android/navigation/GameFragment.kt
+++ app/src/main/java/com/example/android/navigation/GameFragment.kt
@@ -35,26 +35,26 @@ class GameFragment : Fragment() {
     // All questions must have four answers.  We'd want these to contain references to string
     // resources so we could internationalize. (or better yet, not define the questions in code...)
     private val questions: MutableList<Question> = mutableListOf(
-            Question(text = "What is Android Jetpack?",
-                    answers = listOf("all of these", "tools", "documentation", "libraries")),
-            Question(text = "Base class for Layout?",
-                    answers = listOf("ViewGroup", "ViewSet", "ViewCollection", "ViewRoot")),
-            Question(text = "Layout for complex Screens?",
-                    answers = listOf("ConstraintLayout", "GridLayout", "LinearLayout", "FrameLayout")),
-            Question(text = "Pushing structured data into a Layout?",
-                    answers = listOf("Data Binding", "Data Pushing", "Set Text", "OnClick")),
-            Question(text = "Inflate layout in fragments?",
-                    answers = listOf("onCreateView", "onActivityCreated", "onCreateLayout", "onInflateLayout")),
-            Question(text = "Build system for Android?",
-                    answers = listOf("Gradle", "Graddle", "Grodle", "Groyle")),
-            Question(text = "Android vector format?",
+            Question(text = "What part of a plant is clove?",
+                    answers = listOf("bud", "leaf", "stem", "root")),
+            Question(text = "How many petals are there for dayflower?",
+                    answers = listOf("three", "two", "zero", "one")),
+            Question(text = "which is not a unisexual flower?",
+                    answers = listOf("Hibiscus", "Papaya", "Watermelon", "Coconut flower")),
+            Question(text = "Examples of accessory fruits?",
+                    answers = listOf("all of these", "strawberry", "apple", "rose hip")),
+            Question(text = "other names of mimosa pudica?",
+                    answers = listOf("all of these", "shame plant", "sensitive plant", "touch me not")),
+            Question(text = "The dense root systems of ___ forests trap sediments flowing down rivers and off the land in kakinada",
+                    answers = listOf("Mangrove", "Tropical", "Evergreen", "Coniferous"))
+            /*Question(text = "Android vector format?",
                     answers = listOf("VectorDrawable", "AndroidVectorDrawable", "DrawableVector", "AndroidVector")),
             Question(text = "Android Navigation Component?",
                     answers = listOf("NavController", "NavCentral", "NavMaster", "NavSwitcher")),
             Question(text = "Registers app with launcher?",
                     answers = listOf("intent-filter", "app-registry", "launcher-registry", "app-launcher")),
             Question(text = "Mark a layout for Data Binding?",
-                    answers = listOf("<layout>", "<binding>", "<data-binding>", "<dbinding>"))
+                    answers = listOf("<layout>", "<binding>", "<data-binding>", "<dbinding>"))*/
     )
 
     lateinit var currentQuestion: Question
